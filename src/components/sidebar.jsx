b8954d56b0e7611ec4cfb385131e72e20fb5018d
Jacky Lau <jackylau.email@gmail.com>
2020-09-05 01:07:59 -0400

    updated syntax

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/pfbad.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/fullbodyJL.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Jacky Lau</a></h1>
               <span className="email"><i className="icon-mail"></i>   jackylau.email@gmail.com</span>
               <span> </span>
@@ -24,10 +24,12 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li><a href="https://www.facebook.com/jack.e.liu" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://www.instagram.com/sn4ckychan/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                {/* <li><a href="https://www.facebook.com/jack.e.liu" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://www.instagram.com/sn4ckychan/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li> */}
                 <li><a href="https://www.linkedin.com/in/jackylau-nyc/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://www.github.com/JackyLau-nyc" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="mailto:jackylau.email@gmail.com" target="_blank" rel="noopener noreferrer"><i className="icon-mail"></i></a></li>
+
               </ul>
             </nav>
             <div className="colorlib-footer">
